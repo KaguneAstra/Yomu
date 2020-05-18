ecf6d346fb301f31f3104f0bf4409f382f9e7735
PancakeReaper <zombiehkitteh@gmail.com>
2020-05-18 14:25:42 -0700

    base drawer

--- lib/screens/side_drawer/side_drawer.dart
+++ lib/screens/side_drawer/side_drawer.dart
@@ -1,48 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:yomu_for_reddit/screens/side_drawer/account_drawer_header.dart';
 
 import 'account_info_view/account_info_view.dart';
 
 class SideDrawer extends StatelessWidget {
+  List<String> testArray = [
+    "First",
+    "Second",
+    "Third",
+    "First",
+    "Second",
+    "Third",
+    "First",
+    "Second",
+    "Third"
+  ];
+
+  List<ListTile> _makeSubredditList() {
+    List<ListTile> subredditList = [];
+    for (String str in testArray) {
+      subredditList.add(
+        ListTile(
+          title: Text(str),
+          leading: CircleAvatar(
+            radius: 15,
+            backgroundColor: Colors.grey,
+          ),
+          dense: true,
+          onTap: () {
+            print(str);
+          },
+        ),
+      );
+    }
+    return subredditList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          UserAccountsDrawerHeader(
-            accountEmail: Text("Karma: 1337"),
-            accountName: Text("moonlight_splitter"),
-            currentAccountPicture: CircleAvatar(
-              backgroundColor: Colors.grey,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
+          AccountDrawerHeader(
+            Text("u/moonlight_spliter", style: TextStyle(fontSize: 18)),
+            Text("Karma: 1337"),
           ),
-          ListTile(
-            title: Text("First page"),
-            leading: Icon(Icons.home),
-            trailing: Icon(Icons.arrow_drop_down),
-            onTap: () {
-              // Programmatically close the drawer
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text("Account Info"),
-            leading: Icon(Icons.person),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => AccountInfoView()),
-              );
-            },
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(0),
+              children: <Widget>[
+                ListTile(
+                  title: Text("Home"),
+                  leading: Icon(Icons.home),
+                  dense: true,
+                  onTap: () {
+                    // Programmatically close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text("Inbox"),
+                  leading: Icon(Icons.mail),
+                  dense: true,
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text("Saved"),
+                  leading: Icon(Icons.star),
+                  dense: true,
+                  onTap: () {},
+                ),
+                Divider(thickness: 2.0),
+                ..._makeSubredditList(),
+              ],
+            ),
           ),
           Divider(thickness: 2.0),
           ListTile(
-            title: Text("Second page"),
+            title: Text("Settings"),
             leading: Icon(Icons.settings),
-            trailing: Icon(Icons.arrow_drop_down),
+            dense: true,
           ),
         ],
       ),
