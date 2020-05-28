fa926ae33025a2ff3562ebaf71b4fed4e73143a3
PancakeReaper <zombiehkitteh@gmail.com>
2020-05-28 15:15:33 -0700

    inital tab bar view

--- lib/screens/side_drawer/account_info_view/account_info_view.dart
+++ lib/screens/side_drawer/account_info_view/account_info_view.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
 
-class AccountInfoView extends StatelessWidget {
+class AccountInfoView extends StatefulWidget {
+  @override
+  _AccountInfoViewState createState() => _AccountInfoViewState();
+}
+
+class _AccountInfoViewState extends State<AccountInfoView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text("This is the account info view"),
-        alignment: Alignment.center,
-        color: Colors.red,
+    return DefaultTabController(
+      length: 11,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("u/moonlight_spliter"),
+          // TODO - Setup color to a global theme
+          //backgroundColor: Colors.orange,  // might be able to use default colortheme
+          // TODO - Make this thing horizontally scrollable
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: <Widget>[
+              Text("Overview"),
+              Text("About"),
+              Text("Posts"),
+              Text("Comments"),
+              Text("Saved"),
+              Text("Hidden"),
+              Text("Upvoted"),
+              Text("Downvoted"),
+              Text("Gilded"),
+              Text("Friends"),
+              Text("Blocks"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Text("Overview"),
+            Text("About"), // custom view
+            Text("Posts"), // put kevin's infinitescroller here
+            Text("Comments"), // custom scroller widget
+            Text("Saved"), // put kevin's infinitescroller here
+            Text("Hidden"), // put kevin's infinitescroller here
+            Text("Upvoted"), // put kevin's infinitescroller here
+            Text("Downvoted"), // put kevin's infinitescroller here
+            Text("Gilded"), // put kevin's infinitescroller here
+            Text("Friends"),
+            Text("Blocks"),
+          ],
+        ),
       ),
     );
   }
